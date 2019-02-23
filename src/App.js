d9e41ae771888d8d123c09f4cc2c20b02be58755
Joel Switzer <Joelswitzer1@gmail.com>
2019-02-23 03:31:18 -0600

    Website navigation updated, Mobile & tablet scss added, Main scss updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+// import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { animateScroll as Scroll } from 'react-scroll'
 import HeaderNav from './components/header-nav/HeaderNav'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
@@ -7,13 +8,61 @@ import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import './App.scss'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentSection: 0
+    }
+  }
+
+  // Smooth scroll between sections
+  handleScroll = (e) => {
+    const incriment = document.documentElement.clientHeight
+    const current = this.state.currentSection
+    let temp = current;
+    if (e.deltaY < 0) {
+      // Scroll up'
+      temp -= incriment
+      if (temp < 0) {
+        temp = 0;
+      }
+      Scroll.scrollTo(temp)
+      this.setState({
+        currentSection: temp
+      })
+    }
+    else if (e.deltaY > 0 || e.target.value === 'down') {
+      // Scroll down
+      temp += incriment;
+      if (temp > incriment * 3) {
+        temp = incriment * 3
+      }
+      Scroll.scrollTo(temp)
+      this.setState({
+        currentSection: temp 
+      })
+    }
+
+    if (e.target.value === 'down') {
+
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('wheel', this.handleScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('wheel', this.handleScroll);
+  }
+  scrollListener = React.createRef(); 
+
+
   render() {
     return (
-      <div className="App">
+      <div className="App" ref={this.scrollListener}>
         <header>
-          {/* <HeaderNav /> */}
+          <HeaderNav />
         </header>
-        <div className='home-main'>
+        <section id='home-main'>
           <h1 className='main-header'><span className='highlight'>Joel</span> Switzer</h1>
           <h2 className='sub-header'><span>Web Developer, Programmer, Fast Learner</span></h2>
         
@@ -28,8 +77,17 @@ class App extends Component {
               <FontAwesomeIcon icon={faGithub} size='4x' />
             </a>
           </div>
-        </div>
-
+          <button onClick={this.handleScroll} value='down'>click me</button>
+        </section>
+        <section id='about-me'>
+        
+        </section>
+        <section id='my-portfolio'>
+        
+        </section>
+        <section id='contact-me'>
+        
+        </section>
       </div>
     );
   }
--- src/components/header-nav/HeaderNav.js
+++ src/components/header-nav/HeaderNav.js
@@ -1,21 +1,63 @@
 import React, { Component } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-scroll'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Portrait from '../../images/avatar.png'
+import './HeaderNav.scss'
+
 
 export class HeaderNav extends Component {
+  
+  // Toggle the menu
+  toggleMenu = () => {
+    document.querySelector('.btn-menu').classList.toggle('close')
+    document.querySelector('.menu-overlay').classList.toggle('show')
+    document.querySelector('.branding').classList.toggle('show')
+    document.querySelector('.nav-menu').classList.toggle('show')
+    document.querySelectorAll('.nav-menu li').forEach(item => item.classList.toggle('show'))
+  }
+
   render() {
     return (
-
       <nav className='header-nav'>
-        <ul>
-          <li>Home</li>
-          <li>About Me</li>
-          <li>My Portfolio</li>
-          <li>Contact Me</li>
-        </ul>
-        <div className='btn-menu'>
-          <div className='menu-bar'></div>
-          <div className='menu-bar'></div>
-          <div className='menu-bar'></div>
+        <div className='btn-menu' onClick={this.toggleMenu}>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+        </div>
+        <div className='menu-overlay'>
+          <div className='branding'>
+            <img src={Portrait} className='portrait' alt='portrait'/>
+          </div>
+          <ul className='nav-menu'>
+            <li>
+            <Link to='home-main' 
+                activeClass='active-link'
+                spy={true}
+                smooth={true}
+                duration={500}> Home </Link>
+            </li>
+            <li >
+              <Link to='about-me' 
+                activeClass='active-link'
+                spy={true}
+                smooth={true}
+                duration={500}> About Me </Link>
+            </li>
+            <li>
+              <Link to='my-portfolio' 
+                activeClass='active-link'
+                spy={true}
+                smooth={true}
+                duration={500}> Portfolio </Link>
+            </li>
+            <li>
+              <Link to='contact-me' 
+                activeClass='active-link'
+                spy={true}
+                smooth={true}
+                duration={500}> Contact Me </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     )
